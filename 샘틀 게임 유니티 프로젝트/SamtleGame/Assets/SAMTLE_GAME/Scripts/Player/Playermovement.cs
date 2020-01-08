3ede1cea34745c5ba04fde567c5d285a011e9888
nsih6571 <57556313+nsih6571@users.noreply.github.com>
2020-01-08 16:20:24 +0900

    Revert "Revert "test""
    This reverts commit ae210db6a082e5d42ce5579594db63a528cff585.

--- 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Player/Playermovement.cs
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Player/Playermovement.cs
@@ -20,12 +20,11 @@ public class Playermovement : MonoBehaviour
 
     void Move()
     {
-        Vector2 _playerPos = this.gameObject.transform.position;
-        Vector2 _goalPos = new Vector2(_destX, _destY);
-
+        Vector2 playerPos = this.gameObject.transform.position;
+        Vector2 goalPos = new Vector2(_destX, _destY);
 
         #region 키입력
-        if (_playerPos == _goalPos)
+        if (playerPos == goalPos)
         {
             if (Input.GetKey(KeyCode.W))
                 _destY = Convert.ToInt32(this.gameObject.transform.position.y + 1);
@@ -37,22 +36,19 @@ public class Playermovement : MonoBehaviour
                 _destX = Convert.ToInt32(this.gameObject.transform.position.x + 1);
         }
         #endregion
-
         #region dest 추적
         else
         {
-            this.gameObject.transform.Translate((_goalPos - _playerPos).normalized * 3 * Time.deltaTime);
+            this.gameObject.transform.Translate((goalPos - playerPos).normalized * 3 * Time.deltaTime);
 
-            if (_destY > _playerPos.y || _destX > _playerPos.x)
+            if (_destY > playerPos.y || _destX > playerPos.x)
             {
-                if (_playerPos.x - _goalPos.x > -0.1 && _playerPos.y - _goalPos.y > -0.1)
-                    this.gameObject.transform.position = _goalPos;
+                if (playerPos.x - goalPos.x > -0.1 && playerPos.y - goalPos.y > -0.1)
+                    this.gameObject.transform.position = goalPos;
             }
-
-            else if ((_playerPos.x - _goalPos.x < 0.1 && _playerPos.y - _goalPos.y < 0.1))
-                this.gameObject.transform.position = _goalPos;
+            else if (playerPos.x - goalPos.x < 0.1 && playerPos.y - goalPos.y < 0.1)
+                this.gameObject.transform.position = goalPos;
         }
         #endregion
-
     }
 }
