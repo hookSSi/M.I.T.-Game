be04990ce65d65c991448c5b1e5e813a0c4a21a5
hookSSi <sounghoo12@naver.com>
2020-01-09 23:39:47 +0900

    * 인트로 애니메이션, 텍스트, 사운드 출력 틀 준비됨
    * 추가 기타 사항
        TextMeshPro로 만든 한글 폰트가 있음
        2D Image 관련 패키지가 없어서 다운로드 받음

---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/UI/AnimImage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AnimImage : Image
+{
+    [Header("애니메이션 설정")]
+    [SerializeField]
+    [Tooltip("애니메이션 스프라이트")]
+    private Sprite[] _sprites;
+    [SerializeField]
+    [Tooltip("애니메이션 장면 전환 딜레이")]
+    private float _delay;
+    
+    public bool _isLoop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayAnim(_delay));
+    }
+
+    IEnumerator PlayAnim(float delay)
+    {   
+        int index = 0;
+        int length = _sprites.Length;
+
+        while(true)
+        {
+            if(_isLoop)
+            {
+                this.sprite = _sprites[index];
+                yield return new WaitForSeconds(delay);
+
+                index = (index + 1)  % length;
+            }
+            else
+                yield return null;
+        }
+    }
+}
---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/UI/DialogBox.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class DialogBox : MonoBehaviour
+{
+	private TMP_Text _textComponent;
+	[Tooltip("텍스트 출력 효과음")]
+	public AudioClip _typingSound;
+	[Tooltip("텍스트 출력 지연시간")]
+	[SerializeField]
+	private float _delay;
+    public bool _isTextChanged;
+
+	private void Awake ()
+	{
+		_textComponent = gameObject.GetComponent<TMP_Text>();
+	}
+
+	private void Start() 
+	{
+		StartCoroutine(RevealCharacters(_textComponent, _delay));
+	}
+
+	private void OnEnable() 
+	{
+		TMPro_EventManager.TEXT_CHANGED_EVENT.Add(ON_TEXT_CHANGED);
+	}
+
+	private void OnDisable() 
+	{
+		TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(ON_TEXT_CHANGED);
+	}
+
+	void ON_TEXT_CHANGED(Object obj)
+	{
+		_isTextChanged = true;
+	}
+
+	void PlaySound()
+	{
+		
+	}
+
+	/// <summary>
+	/// Method revealing the text one character at a time.
+	/// </summary>
+	/// <returns></returns>
+	IEnumerator RevealCharacters(TMP_Text textComponent, float delay)
+	{
+		textComponent.ForceMeshUpdate();
+
+		TMP_TextInfo textInfo = textComponent.textInfo;
+
+		int totalVisibleCharacters = textInfo.characterCount; // Get # of Visible Character in text object
+		int visibleCount = 0;
+
+		while (true)
+		{
+			if (_isTextChanged)
+			{
+				totalVisibleCharacters = textInfo.characterCount; // Update visible character count.
+				_isTextChanged = false; 
+			}
+
+			if (visibleCount > totalVisibleCharacters)
+			{
+				yield break;
+			}
+
+			PlaySound();
+			textComponent.maxVisibleCharacters = visibleCount; // How many characters should TextMeshPro display?
+			yield return new WaitForSeconds(delay);
+			
+			visibleCount += 1;
+		}
+	}
+}
