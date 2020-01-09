92c640dada97c70c0e5268e143ba24c04ccf800d
aycaramba <rjsxkr1122@naver.com>
2020-01-09 17:46:05 +0900

    3D 이동 다시 만듬

---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Stage3/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float lookSensitivity;
+
+    [SerializeField]
+    private float cameraRotationLimit;
+
+    private float currentCameraRotationX = 0;
+
+    [SerializeField]
+    private Camera camera;
+
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        CameraRotation();
+        CharacterRotation();
+    }
+
+    private void Move()
+    {
+
+        float moveDirX = Input.GetAxisRaw("Horizontal");
+        float moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * moveDirX;
+        Vector3 moveVertical = transform.forward * moveDirZ;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
+
+        rigidbody.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
+    //마우스 좌우, 캐릭터와 함깨 돌아감
+    private void CharacterRotation()
+    {
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 characterRotationY = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+
+        rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(characterRotationY));
+        Debug.Log(rigidbody.rotation);
+        Debug.Log(rigidbody.rotation.eulerAngles);
+    }
+
+    //마우스 상하, 카메라만 돌아감
+    private void CameraRotation()
+    {
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+        float cameraRotationX = xRotation * lookSensitivity;
+
+        currentCameraRotationX -= cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        camera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+
+}
