e3f34ace57b4d4a798192d0f1924b1bb50a6770f
nsih6571 <57556313+nsih6571@users.noreply.github.com>
2020-01-12 19:19:41 +0900

    playermovement
    예시

--- 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Player/Playermovement.cs
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Player/Playermovement.cs
@@ -5,13 +5,22 @@ using System;
 
 public class Playermovement : MonoBehaviour
 {
+
     public static int _destX;
     public static int _destY;
+
+    //sounds
+    public string walkSound;
+    private AudioManager theAudio;
+    //
+
 
     private void Start()
     {
         _destX = Convert.ToInt32(this.gameObject.transform.position.x);
         _destY = Convert.ToInt32(this.gameObject.transform.position.y);
+
+        theAudio = FindObjectOfType<AudioManager>();
     }
     void Update()
     {
@@ -23,7 +32,7 @@ public class Playermovement : MonoBehaviour
         Vector2 playerPos = this.gameObject.transform.position;
         Vector2 goalPos = new Vector2(_destX, _destY);
 
-        #region 키입력
+        #region 키입력 dest 설정
         if (playerPos == goalPos)
         {
             if (Input.GetKey(KeyCode.W))
@@ -36,18 +45,37 @@ public class Playermovement : MonoBehaviour
                 _destX = Convert.ToInt32(this.gameObject.transform.position.x + 1);
         }
         #endregion
-        #region dest 추적
+
+
+        #region dest 추적 실제 움직임
         else
         {
-            this.gameObject.transform.Translate((goalPos - playerPos).normalized * 3 * Time.deltaTime);
+            this.gameObject.transform.Translate( ( goalPos - playerPos ).normalized * 2 * Time.deltaTime );
 
             if (_destY > playerPos.y || _destX > playerPos.x)
             {
                 if (playerPos.x - goalPos.x > -0.1 && playerPos.y - goalPos.y > -0.1)
+                {
                     this.gameObject.transform.position = goalPos;
+                }
             }
             else if (playerPos.x - goalPos.x < 0.1 && playerPos.y - goalPos.y < 0.1)
+            {
                 this.gameObject.transform.position = goalPos;
+            }
+
+            #region walk sound
+            theAudio.Play(walkSound);
+
+            theAudio.SetVolume(walkSound, 0.5f);
+
+            /*
+            theAudio.Stop(walkSound);
+            theAudio.SetLoop(walkSound);
+            theAudio.SetLoopCencle(walkSound);
+            */
+
+            #endregion
         }
         #endregion
     }
