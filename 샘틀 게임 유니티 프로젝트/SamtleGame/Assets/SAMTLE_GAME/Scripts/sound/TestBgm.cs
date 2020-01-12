25ddf2bc87a6f9f63fcae280ad135232a7d54a0b
nsih6571 <57556313+nsih6571@users.noreply.github.com>
2020-01-12 21:15:16 +0900

    sounds manager. 예시를 위한 스크립트 추가 수정 3개

---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/sound/TestBgm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBgm : MonoBehaviour
+{
+    BgmManager BGM;
+
+    public int playMusicTrack;
+
+    void Start()
+    {
+        BGM = FindObjectOfType<BgmManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            BGM.Play(playMusicTrack);
+            this.gameObject.SetActive(false);
+            
+        }
+    }
+}
