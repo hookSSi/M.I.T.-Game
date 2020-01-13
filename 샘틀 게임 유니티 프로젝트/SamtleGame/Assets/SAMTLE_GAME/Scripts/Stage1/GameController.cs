a535655d7f84ab5d9a29ed71ba9ea0321fd0de78
hookSSi <sounghoo12@naver.com>
2020-01-13 14:49:24 +0900

    주성현 폴더 정리

---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Stage1/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Text textScore;
+    public GameObject enemy;
+    private float spawnDelay = 0.8f;
+
+    private int myScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        int randomSpawnDelay = UnityEngine.Random.Range(2, 5);
+        int randomEnemyNumber = UnityEngine.Random.Range(1, 3);
+        for (int i = 0; i < randomEnemyNumber; i++)
+        {
+            int randomEnemyDirection = UnityEngine.Random.Range(1, 3);
+            Vector3 spawnPosition = new Vector3(0, -1.724f, 0);
+            switch (randomEnemyDirection)
+            {
+                case 1:
+                    spawnPosition.x = 10.55f;   // 오른쪽
+                    Instantiate(enemy, spawnPosition, Quaternion.Euler(0, 180, 0));
+                    break;
+                case 2:
+                    spawnPosition.x = -10.55f;  // 왼쪽
+                    Instantiate(enemy, spawnPosition, Quaternion.Euler(0, 0, 0));
+                    break;
+            }
+            yield return new WaitForSeconds(spawnDelay);
+        }
+        yield return new WaitForSeconds(randomSpawnDelay);
+        StartCoroutine(SpawnEnemy());
+    }
+
+    public void RisingScore(int score)
+    {
+        myScore += score;
+        textScore.text = "SCORE : " + myScore.ToString();
+    }
+}
