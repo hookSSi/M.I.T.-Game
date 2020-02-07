e50d4ea6a7fde3ea0c2b8f156c742f8e5260f188
jandy14 <seonguk14@changwon.ac.kr>
2020-02-07 16:24:01 +0900

    스테이지 클리어 후 계단을 내려가기 위한 애니메이션 및 스크립트

---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Stage1/PlayerAnimController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimController : MonoBehaviour
+{
+	public Transform stair;
+
+	public void GoToStair(float distance)
+	{
+		float before = stair.position.x - transform.position.x;
+		ForcedMove(new Vector3(distance, 0, 0));
+		float after = stair.position.x - transform.position.x;
+		if (before * after <= 0)
+		{
+			transform.position = new Vector3(stair.position.x, transform.position.y,transform.position.z);
+		}
+	}
+
+	public void ForcedMove(string xyz)
+	{
+		string[] value = xyz.Split(new char[] { ',', ' ', ':', '/' });
+		ForcedMove(new Vector3(float.Parse(value[0]), float.Parse(value[1]), float.Parse(value[2])));
+	}
+	public void ForcedMove(Vector3 distance)
+	{
+		transform.position += distance;
+	}
+}
