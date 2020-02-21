876b515918b33fce7e276589dae33a3a8c017171
jandy14 <seonguk14@changwon.ac.kr>
2020-02-21 14:00:27 +0900

    stage2 공대관 위치에따라 그리는 순서를 조정하게 스크립트 추가(player and bush)

---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Stage2/CalculateOrderInLayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateOrderInLayer : MonoBehaviour
+{
+	public int offset = 0;
+	public bool isWorkedOnce = true;
+
+	private Renderer _renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+		GetComponent<Renderer>().sortingOrder = (int)(transform.position.y * -100) + offset;
+		if(!isWorkedOnce)
+		{
+			_renderer = GetComponent<Renderer>();
+		}
+		else
+		{
+			Destroy(this);
+		}
+    }
+	private void Update()
+	{
+		_renderer.sortingOrder = (int)(transform.position.y * -100) + offset;
+	}
+}
