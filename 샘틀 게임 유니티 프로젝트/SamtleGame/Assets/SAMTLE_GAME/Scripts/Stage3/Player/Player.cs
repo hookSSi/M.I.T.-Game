ae57b9113448803ed87ecc131d725af1bfbcff25
hookSSi <sounghoo12@naver.com>
2020-02-27 19:45:30 +0900

    * 개요 - 스테이지 3 플레이어 의자에 앉았다, 일어섰다 애니메이션 적용 - 그에 따른 스테이지 3 플레이어 관련 스크립트 일부 수정 (앞으로도 더 수정할 게 있음)

---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Stage3/Player/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MIT.SamtleGame.Stage3
+{
+    public class Player : MonoBehaviour
+    {
+        public GameObject _playerModel;
+        public PlayerController3D _controller;
+    }   
+}
