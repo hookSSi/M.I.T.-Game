58473e945d164997a644e3e3e6ff382d4dc199f8
jandy14 <seonguk14@changwon.ac.kr>
2020-03-02 14:36:05 +0900

    NPCController 캐릭터 이동 구현

--- 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Stage3/NPCController.cs
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Stage3/NPCController.cs
@@ -31,28 +31,48 @@ public class NPCController : MonoBehaviour
 			Move(new Vector3(2.91f, 0f, 5.84f));
 		}
     }
-	void Move(Vector3 pos)
+	public void Move(Vector3 pos)
 	{
 		StartCoroutine(MoveTo(pos));
 		StartCoroutine(Rotate(pos));
 	}
 	IEnumerator MoveTo(Vector3 pos)
 	{
+		_isworking = true;
 		Vector3 startPos = transform.position;
-		_rb.velocity = (pos - startPos).normalized * speed;
-		_anim.SetFloat("Blend", 0.5f);
 		while ((transform.position - pos).magnitude > 0.1f)
 		{
+			if(_rb.velocity.magnitude < speed)
+				_rb.AddForce((pos - startPos).normalized * speed);
+			_anim.SetFloat("Blend", 0.5f * _rb.velocity.magnitude / speed);
 			yield return null;
 		}
 		_rb.velocity = Vector3.zero;
+		//after walk
+		float timer = 0;
+		float breakTime = 0.4f;
+		while(timer < 0.4f)
+		{
+			_anim.SetFloat("Blend", 0.5f * (breakTime - timer) / breakTime);
+			timer += Time.deltaTime;
+			yield return null;
+		}
 		_anim.SetFloat("Blend", 0f);
-		_anim.SetTrigger("LookAround");
+		_isworking = false;
 	}
 	IEnumerator Rotate(Vector3 pos)
 	{
-		transform.LookAt(pos);
-		yield return null;
-	}
+		float startAngle = transform.eulerAngles.y;
+		float angle = Vector3.Angle(pos - transform.position, transform.TransformDirection(Vector3.forward)) + startAngle;
 
+		float timer = 0;
+
+		while (Mathf.Abs(transform.eulerAngles.y - angle) > 0.1f)
+		{
+			transform.eulerAngles = Vector3.up * Mathf.LerpAngle(startAngle, angle, timer / 0.5f);
+			timer += Time.deltaTime;
+			yield return null;
+		}
+		transform.eulerAngles = Vector3.up * angle;
+	}
 }
