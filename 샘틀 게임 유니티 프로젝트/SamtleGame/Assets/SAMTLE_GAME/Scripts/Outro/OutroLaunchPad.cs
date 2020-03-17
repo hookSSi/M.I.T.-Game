4e4226822e4ac18777962f7610fc8bfc80761ed4
jandy14 <seonguk14@changwon.ac.kr>
2020-03-17 14:20:56 +0900

    아웃트로 데코

---
+++ 샘틀 게임 유니티 프로젝트/SamtleGame/Assets/SAMTLE_GAME/Scripts/Outro/OutroLaunchPad.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MIT.SamtleGame.Stage3;
+
+public class OutroLaunchPad : MonoBehaviour
+{
+	public MusicAnalyzer _musicAnalyzer;
+	public List<Pad> _pads = new List<Pad>();
+	public AudioSource audio;
+	public float _threshold;
+	// Start is called before the first frame update
+	void Start()
+	{
+		_musicAnalyzer = GetComponent<MusicAnalyzer>();
+		this.gameObject.GetComponentsInChildren(_pads);
+		PlayLaunchPad();
+	}
+
+	private void PlayLaunchPad()
+	{
+		StartCoroutine(PadLightLoop());
+	}
+
+	IEnumerator PadLightLoop()
+	{
+		_musicAnalyzer.SetMusic(audio);
+
+		while (true)
+		{
+			_musicAnalyzer.AnalyzeMusic();
+			float[] spectrum = _musicAnalyzer.GetSpectrum();
+			bool[] checks = new bool[_pads.Count];
+
+			for (int i = 0; i < spectrum.Length; i++)
+			{
+				if (spectrum[i] > _threshold)
+				{
+					checks[i % _pads.Count] = true;
+				}
+			}
+
+			for (int i = 0; i < checks.Length; i++)
+			{
+				if (checks[i])
+					_pads[i].Press();
+			}
+
+			yield return null;
+		}
+
+		yield break;
+	}
+}
